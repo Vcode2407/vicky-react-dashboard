7c2e3a1283e499f8c5f590b00d5f39730ca9d1d7
Vinay Kumar <vkslog69@gmail.com>
2026-01-18 23:52:47 +0530

    Final clean Vicky React Dashboard v1.0

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -8,7 +8,7 @@ export default function ProductsPage() {
   return (
     <>
       <Helmet>
-        <title> Products | Minimal UI </title>
+        <title> Products | React UI </title>
       </Helmet>
 
       <ProductsView />
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -8,7 +8,7 @@ export default function UserPage() {
   return (
     <>
       <Helmet>
-        <title> User | Minimal UI </title>
+        <title> User | React UI </title>
       </Helmet>
 
       <UserView />
