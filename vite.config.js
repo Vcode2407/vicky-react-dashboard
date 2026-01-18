fdc3c32b594407f0bcc1b1acef396749253bbb8a
Vinay Kumar <vkslog69@gmail.com>
2026-01-19 00:29:59 +0530

    Fix: Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import checker from 'vite-plugin-checker';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
+  base: '/vicky-react-dashboard/',
   plugins: [
     react(),
     checker({
